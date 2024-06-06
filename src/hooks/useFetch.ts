45dac52712a8551d6ed291724d378c357197cae9
Anshuman Gupta <anshuman11197@gmail.com>
2024-06-06 13:23:55 +1000

    api integration

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,45 @@
+'use client';
+
+import React from 'react';
+import { APIResponseProps } from '@/types/APIResponsePropType';
+
+export function useFetch(url: string) {
+
+    // Goals state holds the fetched data
+    const [goals, setGoals] = React.useState<APIResponseProps[]>([]);
+
+    // Error state holds the error message if any
+    const [error, setError] = React.useState<string>("");
+
+    // Loading state holds the loading state
+    const [loading, setLoading] = React.useState<boolean>(true);
+
+    // UseEffect to fetch data everytime the url changes
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try{
+                setLoading(true);
+                const response = await fetch(url);
+                const data = await response.json();
+                if(response.ok){
+                    setGoals(data);
+                    setLoading(false);
+                    setError("");
+                }else{
+                    throw new Error("Error Fetching Data. Please try again later.");
+                }
+                
+            }catch(error){
+                window.alert(error);
+            }finally{
+                setLoading(false);
+            }
+        }
+        
+
+        fetchData();
+    }, [url])
+
+    return { goals, loading, error };
+   
+};
